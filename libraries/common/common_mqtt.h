ca7fbc679ca31d6e067fe941700869077e8dd274
Michael Wyraz <michael@wyraz.de>
2019-10-07 20:08:59 +0200

    common_libs aktualisiert

--- libraries/common/common_mqtt.h
+++ libraries/common/common_mqtt.h
@@ -1,4 +1,9 @@
+#define MQTT_KEEPALIVE 10
+
 #include <PubSubClient.h>
+
+extern const char* MQTT_FALSE = "0";
+extern const char* MQTT_TRUE = "1";
 
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
@@ -17,8 +22,22 @@ void connect_mqtt(char* MQTT_TOPIC) {
   while (!mqttClient.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
+#if defined(MQTT_ALIVE_TOPIC)
+    byte mac[6];
+    WiFi.macAddress(mac);
+    char aliveTopicId[7];
+    sprintf(aliveTopicId, "%02X%02X%02X", mac[3], mac[4], mac[5]);
+    char aliveTopic[255];
+    sprintf(aliveTopic, MQTT_ALIVE_TOPIC, aliveTopicId);
+    if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD, aliveTopic, 1, 1, MQTT_FALSE)) {
+      Serial.println("connected with last will and testament enabled");
+      mqttClient.publish(aliveTopic,MQTT_TRUE,1);
+      Serial.print("Sent alive message to ");
+      Serial.println(aliveTopic);
+#else
     if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {
       Serial.println("connected");
+#endif
       if (MQTT_TOPIC && mqttClient.subscribe(MQTT_TOPIC,1)) {
         Serial.print("Subscribed to ");
         Serial.println(MQTT_TOPIC);
@@ -29,6 +48,7 @@ void connect_mqtt(char* MQTT_TOPIC) {
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
+      wdt_reset();
     }
   }
 }
